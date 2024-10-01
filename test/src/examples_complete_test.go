13184b93ad7ecb628bd43c3626483e9263efc189
Brent Farand <farandb@macewan.ca>
2024-10-01 14:50:52 +0000

    chore: merge changes from upstream, OCP-1225

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -1,37 +1,47 @@
 package test
 
 import (
-	"math/rand"
-	"strconv"
-	"testing"
-	"time"
-
+	"github.com/gruntwork-io/terratest/modules/logger"
+	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"testing"
 )
 
-// Test the Terraform module in examples/complete using Terratest.
-func TestExamplesComplete(t *testing.T) {
-	t.Parallel()
+func cleanup(t *testing.T, terraformOptions *terraform.Options, tempTestFolder string) {
+	terraform.Destroy(t, terraformOptions)
+}
 
-	rand.Seed(time.Now().UnixNano())
+func applyPolicies(t *testing.T, tempTestFolder string, policyFile string) {
+	randID := strings.ToLower(random.UniqueId())
+	attributes := []string{randID}
 
-	randId := strconv.Itoa(rand.Intn(100000))
-	attributes := []string{randId}
-
+	varFiles := []string{"fixtures.us-east-2.tfvars"}
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
-		TerraformDir: "../../examples/complete",
+		TerraformDir: tempTestFolder,
 		Upgrade:      true,
 		// Variables to pass to our Terraform code using -var-file options
-		VarFiles: []string{"fixtures.us-east-2.tfvars"},
+		VarFiles: varFiles,
 		Vars: map[string]interface{}{
 			"attributes": attributes,
+			"list_config_paths": []string{policyFile},
 		},
+	}
+	// Keep the output quiet
+	if !testing.Verbose() {
+		terraformOptions.Logger = logger.Discard
 	}
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer terraform.Destroy(t, terraformOptions)
+	defer cleanup(t, terraformOptions, tempTestFolder)
 
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
@@ -45,4 +55,29 @@ func TestExamplesComplete(t *testing.T) {
 	organizationsPolicyArn := terraform.Output(t, terraformOptions, "organizations_policy_arn")
 	// Verify we're getting back the outputs we expect
 	assert.Contains(t, organizationsPolicyArn, "/service_control_policy/"+organizationsPolicyId)
+}
+
+
+// Test the Terraform module in examples/complete using Terratest.
+func TestExamplesComplete(t *testing.T) {
+	rootFolder := "../../"
+	terraformFolderRelativeToRoot := "examples/complete"
+
+	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, rootFolder, terraformFolderRelativeToRoot)
+	defer os.RemoveAll(tempTestFolder)
+
+	catalogRegEx, e := regexp.Compile("^.+\\.(yaml)$")
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	e = filepath.Walk(path.Join(path.Dir(rootFolder), "./catalog"), func(path string, info os.FileInfo, err error) error {
+		if err == nil && catalogRegEx.MatchString(info.Name()) {
+			applyPolicies(t, tempTestFolder, path)
+		}
+		return nil
+	})
+	if e != nil {
+		log.Fatal(e)
+	}
 }
